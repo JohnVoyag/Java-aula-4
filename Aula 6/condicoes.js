20ecb2ba997b5dbf6f321c5e95abcb3580761b73
Emanuel França <emanuel.anderson00@gmail.com>
2022-07-04 21:17:18 -0300

    Condição simples
    Condição simples usando "if" para determinar velocidade e gerando resultado caso o limite seja ultrapassado, gerando um aviso de multado.

---
+++ Aula 6/condicoes.js
@@ -0,0 +1,6 @@
+var vel = 10
+console.log(`A velocidde do seu carro é ${vel}km/h`)
+if (vel > 60){ // condição simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
